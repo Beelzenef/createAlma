e457e492b8442db9d21e9cde49690d7456ad8d0b
Elena <elena.guzbla@gmail.com>
2016-12-09 13:45:35 +0100

    Correccion de tablas e insercion de datos

--- ejemplos/padron.sql
+++ ejemplos/padron.sql
@@ -1,14 +1,21 @@
+-- Usando base de datos PADRON
 use padron;
 
+-- Eliminando tablas en orden inverso, que no afecta a claves ajenas
+drop table if exists propietarios;
+drop table if exists habitante;
+drop table if exists vivienda;
 drop table if exists municipio;
 
+-- Creacion de tabla MUNICIPIO
 create table municipio (
   	codigoprovincia char(2),
   	codigomunicipio char(3),
   	nombre varchar(50) not null,
     		primary key (codigoprovincia, codigomunicipio)
   );
-
+  
+-- Datos para tabla MUNICIPIO
 insert into municipio (codigoprovincia, codigomunicipio, nombre) values (
     "29", "005", "Malaga - Portada Alta"
   );
@@ -19,41 +26,51 @@ insert into municipio (codigoprovincia, codigomunicipio, nombre) values (
     "29", "010", "Malaga - Cruz Humilladero"
   );
 
-drop table if exists vivienda;
-
+-- Creacion de tabla VIVIENDA
 create table vivienda (
   	nrc char(50),
   	direccion varchar(50) not null,
-  	municipioVivienda char(5) not null,
+  	provinciaVivienda char(2) not null,
+  	municipioVivienda char(3) not null,
     		primary key (nrc),
-    		foreign key (municipioVivienda) references municipio (codigoprovincia, codigomunicipio)
+    		foreign key (provinciaVivienda, municipioVivienda) references municipio (codigoprovincia, codigomunicipio)
 			on update cascade
 			on delete restrict
 );
-  
-drop table if exists habitante;
 
+-- Datos para tabla VIVIENDA
+insert into vivienda values ("00001", "Calle 001", "29", "005");
+insert into vivienda values ("00002", "Calle 002", "29", "007");
+insert into vivienda values ("00003", "Calle 003", "29", "010");
+
+-- Creacion de tabla HABITANTE
 create table habitante (
-	numid int,
+	numid char(10),
   	nombrehab varchar(50) not null,
-  	fechanacimiento char(10) not null,
+  	fechanacimiento date not null,
   	codigovivienda char(50) not null,
-  	provinciahab char(2) not null,
-  	municipiohab char(50) not null,
-    	primary key (numid),
-	foreign key (provinciahab, municipiohab) references municipio (codigoprovincia, codigomunicipio)
+  	cf char(10) not null,
+	primary key (numid),
+	foreign key (codigovivienda) references vivienda (nrc)
 		on update cascade
 		on delete restrict,
-	foreign key (codigovivienda) references vivienda (nr)
+	foreign key (cf) references habitante (numid)
 		on update cascade
 		on delete restrict
 );
 
-drop table if exists propietarios;
+-- Datos para tabla HABITANTE
 
+insert into habitante values ("001", "Elena", "1993-06-12", "00001", "001");
+insert into habitante values ("002", "Colophonius", "1913-04-12", "00001", "001");
+insert into habitante values ("003", "Laín Coubert", "1989-09-12", "00001", "001");
+insert into habitante values ("004", "Dresden", "1982-05-12", "00002", "004");
+insert into habitante values ("005", "Capitan Osoazul", "1978-01-12", "00003", "005");
+
+-- Creacion de tabla PROPIETARIOS
 create table propietarios (
 	codvivienda char(50),
-	codhabitante int,
+	codhabitante char(10),
 		primary key (codvivienda, codhabitante),
 		foreign key (codvivienda) references vivienda (nrc)
 			on update cascade
@@ -61,4 +78,15 @@ create table propietarios (
 		foreign key (codhabitante) references habitante (numid)
 			on update cascade
 			on delete restrict
-	);
+);
+
+-- Datos para tabla PROPIETARIOS
+insert into propietarios values ("00001", "78844445B");
+
+-- Actualizando un codigo de municipio en tabla MUNICIPIO
+select * from municipio;
+update municipio set codigomunicipio = '011' where codigomunicipio = '010';
+select * from municipio;
+select * from vivienda;
+
+select * from habitante;
