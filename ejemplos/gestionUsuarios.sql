04f45a927771faa1df42f41a4ccfc82d8ff38f48
Elena G <elena.guzbla@gmail.com>
2017-03-10 13:44:51 +0100

    Gestion de privilegios

---
+++ ejemplos/gestionUsuarios.sql
@@ -0,0 +1,22 @@
+-- Mostrando 
+show grants;
+select * from information_schema.USER_PRIVILEGES;
+set password = password('claveSegura');
+
+-- WHO AM I? 24601!!!
+select current_user();
+select user();
+
+-- ¿Cómo conceder permisos?
+grant select on liga.* to alumno@’%’;
+
+-- ¿Cómo conceder permisos con la creación de usuarios? En una misma línea:
+grant select on liga.* to alumnito@’%’ identified by ‘1234’;
+
+show grants;
+show databases;
+use liga;
+select * from equipo;
+
+-- ¿Cómo revocar permisos?
+revoke select on liga.* from alumnito@'%';
