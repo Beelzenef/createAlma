ea387ceccfaedb81332a0a726833332febfcfdf5
Elena G <elena.guzbla@gmail.com>
2017-03-14 10:10:40 +0100

    Entregando y eliminando permisos a Otto

--- ejemplos/gestionUsuarios.sql
+++ ejemplos/gestionUsuarios.sql
@@ -21,3 +21,29 @@ select * from equipo;
 -- ¿Cómo revocar permisos?
 revoke select on liga.* from alumnito@'%';
 revoke all privileges, grant optiom from usuario@'%';
+
+-- Jugando con los permisos de Otto
+grant select on liga.jugador to otto@'%';
+show grants;
+show databases;
+use liga;
+show tables;
+select * from jugador;
+select * from equipo;
+grant select (nombre, id) on liga.equipo to otto;
+select id, nombre from equipo;
+revoke select (nombre) on liga.equipo from otto;
+
+grant select (nombre), update (web_oficial) on liga.equipo to otto;
+update equipo set web_oficial = 'LOL' where id = 9;
+
+revoke all privileges, grant option from otto;
+show grants;
+
+grant all on liga.equipo to otto;
+grant select (id, nombre, salario) on liga.jugador to otto;
+show grants;
+
+grant file on *.* to otto;
+
+select id, nombre, salario from jugador into outfile '/tmp/sueldos.csv' fields terminated by ';';
