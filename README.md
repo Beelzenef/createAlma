cc7493aa012552e8ba9a394ea9567a0ddc93207d
Elena G Blanco <elena.guzbla@gmail.com>
2023-01-07 14:20:42 +0100

    📼 Create database + initial queries

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # createAlma
+
+about storing data and databases
---
+++ astro/queries.sql
@@ -0,0 +1,10 @@
+-- MOVIES
+create table MOVIES(
+ title TEXT(100), year TEXT(4)
+)
+
+insert into MOVIES values
+    ('Carol', '2015'),
+    ('Encanto', '2022')
+
+select * from MOVIES
