c33e3d87464b94572be283183625f96f132fb899
Elena G Blanco <elena.guzbla@gmail.com>
2023-01-13 17:57:53 +0100

    📼 Add exercises template

---
+++ astro/ops.sql
@@ -0,0 +1,112 @@
+--== SELECCIONANDO DATOS
+
+-- Selecciona todas las columnas de la tabla MOVIES
+
+
+-- Selecciona la columna de titulo pero renombrala como 'TITULASO'
+
+
+-- Selecciona las columnas de año y titulo de la tabla MOVIES
+
+
+-- Selecciona las columnas de titulo y año de la tabla MOVIES
+
+
+-- Selecciona las columnas de titulo, genero y año de la tabla MOVIES
+
+
+--== OPERADORES BINARIOS
+
+-- Selecciona todas las peliculas que NO sean del 2021
+
+
+-- Selecciona todas las peliculas posteriores al 2000
+
+
+-- Selecciona todas las películas anteriores al 2019
+
+
+-- Selecciona todas las peliculas entre 1995 y 2000
+
+
+-- Selecciona todas las peliculas del 2020
+
+
+--== OPERADORES COMPLEJOS
+
+--== LIMIT
+-- Selecciona únicamente 10 peliculas
+
+
+-- Selecciona únicamente 3 peliculas
+
+
+
+--== ORDERBY
+-- Selecciona todas las peliculas y ordenalas por año
+
+
+-- Selecciona todas las peliculas y ordenalas por titulo
+
+
+-- Selecciona todas las peliculas y ordenalas por año de forma descendente
+
+
+
+--== ORDERBY + LIMIT
+-- Selecciona el titulo de las 3 peliculas mas antiguas
+
+
+-- Selecciona el titulo de las 3 peliculas mas nuevas
+
+
+--== IN
+-- Selecciona las peliculas 
+
+
+--== CONTAINS
+-- Selecciona las peliculas cuyo titulo contenga la palabra "love"
+
+
+-- Selecciona las peliculas cuyo titulo contenga la palabra "day"
+
+
+--== BETWEEN
+-- Selecciona peliculas en un rango de fechas
+
+
+-- GETTING SPECIFIC DATA
+
+
+--== SUBQUERIES
+
+-- Selecciona todas las peliculas romanticas
+
+
+-- Selecciona todas las peliculas romanticas (con subqueries)
+
+
+-- Selecciona todas las peliculas del 2022 y que sean comedias
+
+
+-- Selecciona todas las peliculas del 2022 y que sean comedias (con subqueries)
+
+
+--== JOIN
+-- Selecciona todas las peliculas y muestra también sus géneros
+
+-- Selecciona todos los alquileres y muestra también el nombre de la persona que los realizó
+
+--== OUTER JOIN
+
+
+--== RIGHT O LEFT JOIN
+
+
+--== UNION
+
+
+--== UNION ALL 
+
+
+--== EXCEPT
