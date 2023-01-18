92f85b2a9886d813b6f89c7a2aa7245cfa0b83e2
Elena G Blanco <elena.guzbla@gmail.com>
2023-01-18 18:27:14 +0100

    📼 More data and solved queries

--- astro/ops.sql
+++ astro/ops.sql
@@ -1,68 +1,66 @@
 --== SELECCIONANDO DATOS
 
 -- Selecciona todas las columnas de la tabla MOVIES
-
+select * from MOVIES
 
 -- Selecciona la columna de titulo pero renombrala como 'TITULASO'
-
+select title as TITULASO from MOVIES
 
 -- Selecciona las columnas de año y titulo de la tabla MOVIES
-
+select year, title from MOVIES
 
 -- Selecciona las columnas de titulo y año de la tabla MOVIES
-
+select title, year from MOVIES
 
 -- Selecciona las columnas de titulo, genero y año de la tabla MOVIES
-
+select title, year, id_genre from MOVIES
 
 --== OPERADORES BINARIOS
 
 -- Selecciona todas las peliculas que NO sean del 2021
-
+select * from MOVIES where year <> 2021
 
 -- Selecciona todas las peliculas posteriores al 2000
-
+select * from MOVIES where year > 2020
 
 -- Selecciona todas las películas anteriores al 2019
-
+select * from MOVIES where year < 2019
 
 -- Selecciona todas las peliculas entre 1995 y 2000
-
+select * from MOVIES where year > 1995 and year < 2000
 
 -- Selecciona todas las peliculas del 2020
-
+select * from MOVIES where year = 2022
 
 --== OPERADORES COMPLEJOS
 
 --== LIMIT
 -- Selecciona únicamente 10 peliculas
-
+select * from MOVIES limit 10
 
 -- Selecciona únicamente 3 peliculas
-
-
+select * from MOVIES limit 3
 
 --== ORDERBY
 -- Selecciona todas las peliculas y ordenalas por año
-
+select * from MOVIES ORDER by year
 
 -- Selecciona todas las peliculas y ordenalas por titulo
-
+select * from MOVIES ORDER by title
 
 -- Selecciona todas las peliculas y ordenalas por año de forma descendente
-
-
+select * from MOVIES ORDER by year DESC
 
 --== ORDERBY + LIMIT
 -- Selecciona el titulo de las 3 peliculas mas antiguas
-
+select * from MOVIES order by year limit 3
 
 -- Selecciona el titulo de las 3 peliculas mas nuevas
-
+select * from MOVIES order by year desc limit 3
 
 --== IN
--- Selecciona las peliculas 
-
+-- Selecciona las peliculas de los años 2015, 2019 y 2022
+select * from MOVIES where year in (2015, 2019, 2022)
 
 --== CONTAINS
 -- Selecciona las peliculas cuyo titulo contenga la palabra "love"
@@ -77,20 +75,26 @@
 
 -- GETTING SPECIFIC DATA
 
+--== SUBQUERIES FOR DIRECTORS
+-- Selecciona todas las peliculas de Spielberg
+select * from MOVIES where id_director = 10
 
---== SUBQUERIES
+-- Selecciona todas las peliculas de Nolan anteriores a 2010
+
+
+--== SUBQUERIES FOR GENRES
 
 -- Selecciona todas las peliculas romanticas
-
+select * from MOVIES where id_genre = 6
 
 -- Selecciona todas las peliculas romanticas (con subqueries)
+select * from MOVIES where id_genre = (select id from GENRES where name = 'Romance')
 
+-- Selecciona todas las peliculas del 2019 y que sean de terror
+select * from MOVIES where id_genre = 5 and year = 2019
 
--- Selecciona todas las peliculas del 2022 y que sean comedias
-
-
--- Selecciona todas las peliculas del 2022 y que sean comedias (con subqueries)
-
+-- Selecciona todas las peliculas del 2019 y que sean de terror (con subqueries)
+select * from MOVIES where id_genre = (select id from GENRES where name = 'Horror') and year = 2019
 
 --== JOIN
 -- Selecciona todas las peliculas y muestra también sus géneros
--- astro/queries.sql
+++ astro/queries.sql
@@ -14,7 +14,7 @@ create table DIRECTORS (
 create table MOVIES(
     id INTEGER PRIMARY KEY NOT NULL,
     title TEXT(100),
-    year TEXT(4),
+    year INTEGER,
     id_genre INTEGER NOT NULL,
     id_director INTEGER NOT NULL,
     FOREIGN KEY (id_genre) REFERENCES GENRES (id),
@@ -34,10 +34,25 @@ create table CLIENTS (
 --drop table GENRES
 --drop table CLIENTS
 
+-- delete from directors
+-- delete from movies
+
 -- INSERTING DATA
 insert into DIRECTORS values
       (1, 'Todd Haynes'),
-      (2, 'Jared Bush')
+      (2, 'Jared Bush'),
+      (3, 'Christopher Nolan'),
+      (4, 'David Fincher'),
+      (5, 'Lilly Wachowski'),
+      (6, 'Stanley Kubrick'),
+      (7, 'Bong Joon-ho'),
+      (8, 'Andrew Stanton'),
+      (9, 'Riddley Scott'),
+      (10, 'Steven Spielberg'),
+      (11, 'Quentin Tarantino'),
+      (12, 'Roland Emmerich'),
+      (13, 'John Madden'),
+      (14, 'Richard Curtis')
 
 insert into GENRES values
       (1, 'Drama'),
@@ -49,12 +64,27 @@ insert into GENRES values
       (7, 'Thriller')
 
 insert into MOVIES values
-    (1, 'Carol', '2015', 4, 2),
-    (2, 'Encanto', '2022', 8, 1)
+    (1, 'Carol', 2015, 6, 2),
+    (2, 'Encanto', 2022, 8, 1),
+    (3, 'Pulp Fiction', 1994, 3, 11),
+    (4, 'Schindler`s list', 1994, 1, 10),
+    (5, 'The Dark Knight', 1994, 3, 3), 
+    (6, 'The day after tomorrow', 2004, 3, 12),
+    (7, 'Matrix', 1999, 3, 5),
+    (8, 'Love actually', 2003, 6, 14),
+    (9, 'Interstellar', 2014, 1, 3), 
+    (10, 'Parasite', 2019, 5, 7), 
+    (11, 'Alien', 1979, 5, 9), 
+    (12, 'WALL-E', 2008, 2, 8), 
+    (13, 'The shining', 1980, 5, 6), 
+    (14, '2001, a space odissey', 1968, 5, 6),
+    (15, 'Independence day', 1996, 3, 12),
+    (16, 'Inception', 2010, 1, 3),
+    (17, 'Shakespeare in love', 1998, 6, 13),
+    (18, 'Fight club', 1999, 3, 4) 
 
 insert into CLIENTS values
     (1, 'Elena G Blanco', date('2000-05-05'))
-
 
 -- GIVE ME ALL YOU GOT
 select * from MOVIES
